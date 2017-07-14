d06e8c89ae1448d449e26f9b0694613356636b6e
Luv <LoveLeAnon@users.noreply.github.com>
2017-07-13 21:45:37 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -30,20 +30,20 @@ public:
         pchMessageStart[2] = 0x03;
         pchMessageStart[3] = 0xdf;
         vAlertPubKey = ParseHex("04f09702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284");
-        nDefaultPort = 12835;
-        nRPCPort = 12832;
+        nDefaultPort = 14839;
+        nRPCPort = 15894;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
         nSubsidyHalvingInterval = 950000;
 
         // Build the genesis block. Note that the output of the genesis coinbase cannot
         // be spent as it did not originally exist in the database.
 
-        const char* pszTimestamp = "Tuesday July 11th 2017 2:16PM";
+        const char* pszTimestamp = "Friday July 14th 2017 9:38PM a new coin was born";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
-        txNew.vout[0].nValue = 5000 * COIN;
+        txNew.vout[0].nValue = 25 * COIN;
         txNew.vout[0].scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
@@ -51,7 +51,7 @@ public:
         genesis.nVersion = 2;
         genesis.nTime    = 1499796923;
         genesis.nBits    = 0x1e0ffff0;
-        genesis.nNonce   = 804458;
+        genesis.nNonce   = 0;
 
         //// debug print
         hashGenesisBlock = genesis.GetHash(); //dont cinnebt out
@@ -66,12 +66,10 @@ public:
         genesis.print();
 
 
-        assert(hashGenesisBlock == uint256("0x00000f18f96ca349919202037b80f7635fc7a1bc5172547cd63f1b8ebf87fd73"));
-        assert(genesis.hashMerkleRoot == uint256("0xc68d2ebb9973d4689afd74de43f4009a3038d347472e3b47a033a9703ae13e23"));
+        assert(hashGenesisBlock == uint256("0x"));
+        assert(genesis.hashMerkleRoot == uint256("0x"));
 
        vSeeds.push_back(CDNSSeedData("162.212.153.103", "162.212.153.103"));
-        //vSeeds.push_back(CDNSSeedData("node.asiccoin.cf", "node.asiccoin.cf"));
-        //vSeeds.push_back(CDNSSeedData("asiccoin.no-ip.org", "asiccoin.no-ip.org"));
 
         base58Prefixes[PUBKEY_ADDRESS] = 50;
         base58Prefixes[SCRIPT_ADDRESS] = 9;
@@ -103,137 +101,3 @@ protected:
     vector<CAddress> vFixedSeeds;
 };
 static CMainParams mainParams;
-
-
-//
-// Testnet (v3)
-//
-class CTestNetParams : public CMainParams {
-public:
-    CTestNetParams() {
-        // The message start string is designed to be unlikely to occur in normal data.
-        pchMessageStart[0] = 0x05;
-        pchMessageStart[1] = 0xfe;
-        pchMessageStart[2] = 0xa9;
-        pchMessageStart[3] = 0x01;
-        vAlertPubKey = ParseHex("04303390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a");
-        nDefaultPort = 11835;
-        nRPCPort = 11832;
-        strDataDir = "testnet";
-
-        // Modify the testnet genesis block so the timestamp is valid for a later start.
-        genesis.nTime = 1499796923;
-        genesis.nNonce = 804458;
-
-
-        //// debug print
-        hashGenesisBlock = genesis.GetHash();
-       while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
-           if (++genesis.nNonce==0) break;
-         hashGenesisBlock = genesis.GetHash();
-       }
-
-        printf("%s\n", hashGenesisBlock.ToString().c_str());
-        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
-        genesis.print();
-
-        assert(hashGenesisBlock == uint256("0x00000f18f96ca349919202037b80f7635fc7a1bc5172547cd63f1b8ebf87fd73"));
-		
-        vFixedSeeds.clear();
-        vSeeds.clear();
-        // vSeeds.push_back(CDNSSeedData("asiccoin.test", "test.asiccoin.org"));
-
-        base58Prefixes[PUBKEY_ADDRESS] = 88;
-        base58Prefixes[SCRIPT_ADDRESS] = 188;
-        base58Prefixes[SECRET_KEY] = 239;
-
-    }
-    virtual Network NetworkID() const { return CChainParams::TESTNET; }
-};
-//static CTestNetParams testNetParams;
-
-
-//
-// Regression test
-//
-class CRegTestParams : public CTestNetParams {
-public:
-    CRegTestParams() {
-        pchMessageStart[0] = 0xfa;
-        pchMessageStart[1] = 0x0f;
-        pchMessageStart[2] = 0xa5;
-        pchMessageStart[3] = 0x5a;
-        nSubsidyHalvingInterval = 150;
-        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 1);
-        genesis.nTime = 1390748221;
-        genesis.nBits = 0x207fffff;
-        genesis.nNonce = 4;
-        hashGenesisBlock = genesis.GetHash();
-        nDefaultPort = 11444;
-        strDataDir = "regtest";
-
-        // debug print
-        hashGenesisBlock = genesis.GetHash();
-        while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
-           if (++genesis.nNonce==0) break;
-            hashGenesisBlock = genesis.GetHash();
-        }
-
-        printf("%s\n", hashGenesisBlock.ToString().c_str());
-        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
-        genesis.print();
-
-        //assert(hashGenesisBlock == uint256("0x));
-
-        vSeeds.clear();  // Regtest mode doesn't have any DNS seeds.
-
-        base58Prefixes[PUBKEY_ADDRESS] = 0;
-        base58Prefixes[SCRIPT_ADDRESS] = 5;
-        base58Prefixes[SECRET_KEY] = 128;
-    }
-
-    virtual bool RequireRPCPassword() const { return false; }
-    virtual Network NetworkID() const { return CChainParams::REGTEST; }
-};
-//static CRegTestParams regTestParams;
-
-static CChainParams *pCurrentParams = &mainParams;
-
-const CChainParams &Params() {
-    return *pCurrentParams;
-}
-
-void SelectParams(CChainParams::Network network) {
-    switch (network) {
-        case CChainParams::MAIN:
-            pCurrentParams = &mainParams;
-            break;
-        //case CChainParams::TESTNET:
-        //    pCurrentParams = &testNetParams;
-        //    break;
-        //case CChainParams::REGTEST:
-        //    pCurrentParams = &regTestParams;
-        //    break;
-        default:
-            assert(false && "Unimplemented network");
-            return;
-    }
-}
-
-bool SelectParamsFromCommandLine() {
-    bool fRegTest = GetBoolArg("-regtest", false);
-    bool fTestNet = GetBoolArg("-testnet", false);
-
-    if (fTestNet && fRegTest) {
-        return false;
-    }
-
-    if (fRegTest) {
-        SelectParams(CChainParams::REGTEST);
-    } else if (fTestNet) {
-        SelectParams(CChainParams::TESTNET);
-    } else {
-        SelectParams(CChainParams::MAIN);
-    }
-    return true;
-}
